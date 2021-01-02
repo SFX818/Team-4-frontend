daab2a0c70027f411ef77ed77a2b40cdd8c80fc3
NatrezC <cnatrez@gmail.com>
2021-01-02 15:09:50 -0600

    post has currentuser info

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { getCurrentUser } from '../services/auth.service'
+
+const Post = () = {
+    const currentUser = getCurrentUser()
+    
+    return(
+
+    )
+}
+
+export default Post;
