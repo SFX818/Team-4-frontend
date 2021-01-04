ac56ae5154440eab945ff8f0746d004188fd18b4
NatrezC <cnatrez@gmail.com>
2021-01-04 10:15:52 -0600

    created a card for posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,21 @@ import React from 'react'
 import { getCurrentUser } from '../services/auth.service'
 
 
+
 const Home = () => {
   const currentUser = getCurrentUser()
 
   return (
     <div>
       {currentUser && (
-        <p>yikes</p>
+        <div className="card">
+          
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+</div>
       )}
     </div>
   )
