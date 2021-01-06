f8f5097c2a2b5f426de2a78305dcba4e0183b273
xlnl <lamanhnle@gmail.com>
2021-01-05 21:19:48 -0500

    working through refactoring home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from 'react';
-// import { Image } from 'cloudinary-react';
 
-// renders all the posts on a timeline
+import Posts from './Posts/Posts'
+
 const Home = () => {
-  // const [imageIds, setImageIds] = useState();
-  // const loadImages = async () => {
-  //     try {
-  //         const res = await fetch('/api/images');
-  //         const data = await res.json();
-  //         setImageIds(data);
-  //     } catch (err) {
-  //         console.error(err);
-  //     }
-  // };
-  // useEffect(() => {
-  //     loadImages();
-  // }, []);
+  const [currentId, setCurrentId] = useState(0);
+
   return (
       <div>
           <h1 className="title">Welcome to Petflix</h1>
-          <div className="gallery"></div>
+          <div className="gallery">
+            <Posts setCurrentId={setCurrentId} />
+          </div>
       </div>
   );
   
