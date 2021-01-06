f1a46fd602a1e93b484fe12b620b66be43792642
granddivino <granddivino@gmail.com>
2021-01-05 19:14:30 -0700

    Updated actual Home.js (not my temporary code)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
+import React, { useEffect, useState } from 'react';
+// hook from redux to get actions
+import { useDispatch } from 'react-redux';
+
+import { findAll } from '../actions/posts'
+import Posts from './Posts/Posts'
+
+// renders all the posts on a timeline
 const Home = () => {
-  return <div>HomePage</div>;
-  
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+
+  // successful dispatch
+  useEffect(() => {
+    dispatch(findAll());
+  }, [currentId, dispatch]);
+
+  return (
+      <div>
+          <h1 className="title">Welcome to Petflix</h1>
+          <div className="gallery">
+            <Posts setCurrentId={setCurrentId} />
+          </div>
+      </div>
+  );
 };
 
 export default Home;
-
 
 
 //Currently working out of Homepage to see what I'm doing easier since auth seems to be broken again
