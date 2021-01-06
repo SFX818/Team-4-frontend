38050dcc61cf634ed717de64f0dbde7256efc6c4
NatrezC <cnatrez@gmail.com>
2021-01-05 19:06:36 -0600

    was able to post pic with post not the right way tho but hey it works

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
       {currentUser && (
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." class="card-img-top" alt="..."/>
+          <img src="images/Tony" class="card-img-top" alt="..."/>
             <div class="card-body">
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
