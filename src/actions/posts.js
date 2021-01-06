6d710a7f6f661e8c9594b8eb5da1f6f4f9da1ced
xlnl <lamanhnle@gmail.com>
2021-01-05 21:07:23 -0500

    stubbing out the db functions for CRUD actions for post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -4,3 +4,26 @@ import * as api from '../api/index.js';
 
 // using dispatch/redux-thunk to have async-await properties for the actions 
 // get the response from the API and destructure the data (post)
+export const findAll = () => async (dispatch) => {
+
+};
+
+export const findOne = () => async (dispatch) => {
+
+};
+
+export const createPost = (post) => async (dispatch) => {
+
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+
+};
+
+export const likePost = (id) => async (dispatch) => {
+
+};
+
+export const deletePost = (id) => async (dispatch) => {
+
+}; 
