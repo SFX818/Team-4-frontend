fd1a27f19882b2be920ecc551c3cfbcfc41cdebc
granddivino <granddivino@gmail.com>
2021-01-07 22:29:04 -0700

    Added about page images and css

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import logo from "../css/Petflix_Logo-01.png"
+import Lam from '../css/Lam.jpg'
+import Trez from '../css/Trez.jpg'
+import Gabe from '../css/Gabe.jpg'
 
 const About = () => {
     return (
       <div class="about">
           <div class="aboutapp">
-              {/* <img src={logo} alt="Logo" className="logo"/> */}
+              <h1>About the Developers</h1>
               <p>
                 Text about PetFlix will go here.
               </p>
           </div>
           <div class="developer" style={{ width:"18rem"}}>
-            {/* Lam's Image */}
+            <h3>Lam-Anh Le</h3>
+            <img src={Lam} class="DevImage"/>
             <p>
                I am a data-driven, human-centric design expert with extensive history in communications 
                strategy and programming, eager to apply my passion for public service with my project 
@@ -22,18 +26,25 @@ const About = () => {
             </a>
           </div>
           <div class="developer" style={{ width:"18rem"}}>
-            {/* Trez's Image */}
+            <h3>Trez Crawford</h3>
+            <img src={Trez} class="DevImage"/>
             <p>
-               About Trez
+               I am a data-driven, human-centric design expert with extensive history in communications 
+               strategy and programming, eager to apply my passion for public service with my project 
+               management and problem-solving skills to any challenge.
             </p>
             <a href="https://github.com/NatrezC">
                 Trez's Github
             </a>
           </div>
-          <div class="developer">
-            {/* Gabe's Image */}
+          <div class="developer" style={{ width:"18rem"}}>
+            <h3>Gabriel Go</h3>
+            <img src={Gabe} class="DevImage"/>
             <p>
-               About Gabe
+              As a driven software engineer, I always look for innovative ways to expand my
+              knowledge of programming techniques. My calling lies in front end development as I
+              have a unique vision for aesthetic and conveying my product vision through my work.
+              A keen eye for originality makes me an asset as a developer and team member.
             </p>
             <a href="https://github.com/granddivino">
                 Gabe's Github
