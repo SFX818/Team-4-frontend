f33e54a57c24daab858ea2d7336f9239e4e502c1
granddivino <granddivino@gmail.com>
2021-01-07 23:18:08 -0700

    Added more css to about page, could be temporary -- need confirmation from team

--- src/components/About.js
+++ src/components/About.js
@@ -7,46 +7,56 @@ import Gabe from '../css/Gabe.jpg'
 const About = () => {
     return (
       <div class="about">
+          <div class="mainheader">
+              <u>About the Developers of Petflix</u>
+          </div>
           <div class="aboutapp">
-              <h1>About the Developers</h1>
               <p>
-                Text about PetFlix will go here.
+                Text about PetFlix will go here.Text about PetFlix will go here.Text about PetFlix will go here.
+                Text about PetFlix will go here.Text about PetFlix will go here.Text about PetFlix will go here.
+                Text about PetFlix will go here.Text about PetFlix will go here.Text about PetFlix will go here.
+                Text about PetFlix will go here.Text about PetFlix will go here.Text about PetFlix will go here.
+                Text about PetFlix will go here.Text about PetFlix will go here.Text about PetFlix will go here.
               </p>
           </div>
           <div class="developer" style={{ width:"18rem"}}>
-            <h3>Lam-Anh Le</h3>
+            <div class="name"><h2><u>Lam-Anh Le</u></h2></div>
             <img src={Lam} class="DevImage"/>
-            <p>
+            <div class="aboutdev">
+              <p>
                I am a data-driven, human-centric design expert with extensive history in communications 
                strategy and programming, eager to apply my passion for public service with my project 
                management and problem-solving skills to any challenge.
-            </p>
-            <a href="https://github.com/xlnl">
+              </p>
+            </div>
+            <a href="https://github.com/xlnl" class="gitlink">
                 Lam's Github
             </a>
           </div>
           <div class="developer" style={{ width:"18rem"}}>
-            <h3>Trez Crawford</h3>
+            <div class="name"><h2><u>Trez Crawford</u></h2></div>
             <img src={Trez} class="DevImage"/>
-            <p>
-               I am a data-driven, human-centric design expert with extensive history in communications 
-               strategy and programming, eager to apply my passion for public service with my project 
-               management and problem-solving skills to any challenge.
-            </p>
-            <a href="https://github.com/NatrezC">
+            <div class="aboutdev">
+              <p>
+               My name is Trez. I have not given Gabe my brand statement or what I actually want to go here yet.
+              </p>
+            </div>
+            <a href="https://github.com/NatrezC" class="gitlink">
                 Trez's Github
             </a>
           </div>
           <div class="developer" style={{ width:"18rem"}}>
-            <h3>Gabriel Go</h3>
+            <div class="name"><h2><u>Gabriel Go</u></h2></div>
             <img src={Gabe} class="DevImage"/>
-            <p>
+            <div class="aboutdev">
+              <p>
               As a driven software engineer, I always look for innovative ways to expand my
               knowledge of programming techniques. My calling lies in front end development as I
               have a unique vision for aesthetic and conveying my product vision through my work.
               A keen eye for originality makes me an asset as a developer and team member.
-            </p>
-            <a href="https://github.com/granddivino">
+              </p>
+            </div>
+            <a href="https://github.com/granddivino" class="gitlink">
                 Gabe's Github
             </a>
           </div>
